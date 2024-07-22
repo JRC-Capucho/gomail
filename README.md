5d8ae56412fffce529288685aba36198c7ec43ce
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-07-22 01:17:04 -0300

    feat: testing mock with service campaign

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gomail
+
+## Object
+
+Service to send email in batch, easy management object for the user
---
+++ internal/domain/campaign/campaign_test.go
@@ -0,0 +1,66 @@
+package campaign
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	name     = "Campaign X"
+	content  = "content X"
+	contacts = []string{"email@mail.com", "email2@mail.com"}
+)
+
+func Test_New_Campaign(t *testing.T) {
+	assert := assert.New(t)
+
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_New_Campaign_Id_Not_Nil(t *testing.T) {
+	assert := assert.New(t)
+
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.Id)
+}
+
+func Test_New_Campaign_CreateOn_Must_Be_Now(t *testing.T) {
+	assert := assert.New(t)
+
+	now := time.Now().Add(-time.Minute)
+
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreatedOn, now)
+}
+
+func Test_New_Campaign_Must_Validate_Name(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign("", content, contacts)
+
+	assert.Equal("name is required", err.Error())
+}
+
+func Test_New_Campaign_Must_Validate_Content(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, "", contacts)
+
+	assert.Equal("content is required", err.Error())
+}
+
+func Test_New_Campaign_Must_Validate_contacts(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, []string{})
+
+	assert.Equal("contacts is required", err.Error())
+}
